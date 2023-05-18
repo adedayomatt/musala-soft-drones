3f95212e490698f3f3c1e2d2d4564b3dcd8fe319
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-18 22:21:01 +0100

    Create validator for drone to satisfy conditions for registering a drone

---
+++ src/main/java/com/musala/drones/validators/DroneValidator.java
@@ -0,0 +1,51 @@
+package com.musala.drones.validators;
+
+import com.musala.drones.drone.Drone;
+import com.musala.drones.enums.Model;
+import com.musala.drones.enums.State;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DroneValidator {
+    private Drone drone;
+
+    public Drone getDrone() {
+        return drone;
+    }
+
+    public DroneValidator setDrone(Drone drone) {
+        this.drone = drone;
+        return this;
+    }
+
+    public Boolean serialNumberValid() {
+        return drone.getSerialNumber().matches("^[a-zA-Z]+$")
+                && drone.getSerialNumber().length() <= 100;
+    }
+
+    public Boolean modelValid() {
+        for (Model model : Model.values()) {
+            if (model.name().equals(drone.getModel())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean stateValid() {
+        for (State state : State.values()) {
+            if (state.name().equals(drone.getState())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean weightLimitValid() {
+        return drone.getWeightLimit() <= 500;
+    }
+
+    public Boolean isValid() {
+        return serialNumberValid() && modelValid() && stateValid() && weightLimitValid();
+    }
+}
