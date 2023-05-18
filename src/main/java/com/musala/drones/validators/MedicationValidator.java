d2d63886150be8014d53d122e4daa52df883a941
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-18 22:21:57 +0100

    Create validator for medication to satisfy conditions for loading it to a drone

---
+++ src/main/java/com/musala/drones/validators/MedicationValidator.java
@@ -0,0 +1,30 @@
+package com.musala.drones.validators;
+
+import com.musala.drones.drone.Medication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MedicationValidator {
+    private Medication medication;
+
+    public Medication getMedication() {
+        return medication;
+    }
+
+    public MedicationValidator setMedication(Medication medication) {
+        this.medication = medication;
+        return this;
+    }
+
+    public Boolean nameValid() {
+        return medication.getName().matches("^[a-zA-Z_0-9-]+$");
+    }
+
+    public Boolean codeValid() {
+        return medication.getCode().matches("^[A-Z_0-9]+$");
+    }
+
+    public Boolean isValid() {
+        return nameValid() && codeValid();
+    }
+}
