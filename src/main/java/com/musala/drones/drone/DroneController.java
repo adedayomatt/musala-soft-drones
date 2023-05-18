28f03e1cb3d7b5c0fd929807d91b9d044620dca4
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-18 19:56:09 +0100

    Create drone controller with RESTful mappings

---
+++ src/main/java/com/musala/drones/drone/DroneController.java
@@ -0,0 +1,35 @@
+package com.musala.drones.drone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/drones")
+public class DroneController {
+
+    private final DroneService droneService;
+
+    @Autowired
+    public DroneController(DroneService droneService) {
+        this.droneService = droneService;
+    }
+
+    @PostMapping
+    public Drone registerDrone(@RequestBody Drone drone) {
+        return droneService.registerDrone(drone);
+    }
+
+    @GetMapping
+    public List<Drone> getDrones() {
+        return droneService.getDrones();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Drone> getDrone(@PathVariable("id") Long id) {
+        return droneService.getDrone(id);
+    }
+
+}
