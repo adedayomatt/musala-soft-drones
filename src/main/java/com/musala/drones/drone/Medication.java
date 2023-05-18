1d6c35b442d541a8f1d66771d2fcc4919315a823
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-18 22:19:07 +0100

    Create medication object

---
+++ src/main/java/com/musala/drones/drone/Medication.java
@@ -0,0 +1,41 @@
+package com.musala.drones.drone;
+
+public class Medication {
+    String name;
+    Double weight;
+    String code;
+    String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
