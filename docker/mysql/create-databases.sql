e565cbe4b1b42285add976ba0945c3cab21d4634
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-23 20:21:09 +0100

    ------Discontinued implementation in Java-------

--- docker/mysql/create-databases.sql
+++ docker/mysql/create-databases.sql
@@ -1,5 +1,4 @@
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 
 # create databases
-CREATE DATABASE IF NOT EXISTS `drones`;
-CREATE DATABASE IF NOT EXISTS `drones-test`;
+CREATE DATABASE IF NOT EXISTS `java-drones`;
