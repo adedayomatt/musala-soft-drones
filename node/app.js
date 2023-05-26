c29c0947bffae73b50c1ba0cd9ab9d56be0650ef
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-26 10:42:45 +0100

    Logging every incoming requests and corresponding responses

--- node/app.js
+++ node/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('app/config/config');
+const { requestLogger, handleError } = require("app/middlewares")
+const ServiceResponse = require("app/services/ServiceResponse");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
+
 app.use(cors());
+app.use(requestLogger)
+
 app.use("/", (req, res) => {
-    res.json({ message: " Hello world!!" })
+    (new ServiceResponse(req, res)).error({
+        status_code: 404,
+        message: "Not Found"
+    })
 });
+app.use(handleError)
 
 app.listen(config.server.port, () => {
     console.log(`Listening on port ${config.server.port}`)
