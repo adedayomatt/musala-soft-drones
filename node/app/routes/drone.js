926751e49e405a14679dcd6f064f97840ed3d463
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-26 14:04:04 +0100

    Endpoint to load drone with medications

--- node/app/routes/drone.js
+++ node/app/routes/drone.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { validate } = require("express-validation");
-const validations = require("app/validations")
-const droneController = require("app/controllers/droneController")
+const validations = require("app/validations");
+const { droneExist, droneIsIdle } = require("app/middlewares");
+const droneController = require("app/controllers/droneController");
 
 router.post("/", [validate(validations.droneRegistration)], droneController.registerDrone);
 router.get("/", [], droneController.getDrones);
 router.get("/available", [], droneController.getIdleDrones);
 router.get("/:id", [], droneController.getDrone);
+router.post("/:id/load", [droneExist, droneIsIdle, validate(validations.droneLoad)], droneController.loadDroneWithMedications);
 
 module.exports = router;
