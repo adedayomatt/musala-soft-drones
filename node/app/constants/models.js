4702789eb29db0385de01ddd2b3a4c4714034dd5
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-26 10:44:19 +0100

    Create drone model

---
+++ node/app/constants/models.js
@@ -0,0 +1,9 @@
+const models = {
+    LIGHTWEIGHT: "Lightweight",
+    MIDDLEWEIGHT: "Middleweight",
+    CRUISEWEIGHT: "Cruiserweight",
+    HEAVYWEIGHT: "Heavyweight",
+
+    allModels: () => [models.LIGHTWEIGHT, models.MIDDLEWEIGHT, models.CRUISEWEIGHT, models.HEAVYWEIGHT]
+}
+module.exports = models;
