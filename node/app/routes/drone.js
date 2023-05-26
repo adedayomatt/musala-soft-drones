0f2ed4c8661f0d4b2b5bc3a04ece7057602d310f
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-26 14:46:21 +0100

    Middleware to check medications weight against drone weight limit

--- node/app/routes/drone.js
+++ node/app/routes/drone.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { validate } = require("express-validation");
 const validations = require("app/validations");
-const { droneExist, droneIsIdle } = require("app/middlewares");
+const { droneExist, droneIsIdle, checkMedicationWeight } = require("app/middlewares");
 const droneController = require("app/controllers/droneController");
 
 router.post("/", [validate(validations.droneRegistration)], droneController.registerDrone);
 router.get("/", [], droneController.getDrones);
 router.get("/available", [], droneController.getIdleDrones);
 router.get("/:id", [], droneController.getDrone);
-router.post("/:id/load", [droneExist, droneIsIdle, validate(validations.droneLoad)], droneController.loadDroneWithMedications);
+router.post("/:id/load", [
+    droneExist, droneIsIdle, validate(validations.droneLoad), checkMedicationWeight
+], droneController.loadDroneWithMedications);
 
 module.exports = router;
