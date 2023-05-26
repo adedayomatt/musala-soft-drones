ab4213f8fedaf9e129b8311250902322bff98b62
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-26 10:40:51 +0100

    Create request log model

---
+++ node/app/models/requestlog.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class RequestLog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Client, Transaction }) {
+      // define association here
+    }
+  }
+  RequestLog.init({
+    endpoint: DataTypes.STRING,
+    method: DataTypes.STRING,
+    request: DataTypes.TEXT,
+    response: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'RequestLog',
+  });
+  return RequestLog;
+};
