cb0b48425a886b96d5345988b53cbcd18d331bd1
Adedayo Matthews <adedayo.kayode@konga.com>
2023-05-29 00:06:25 +0100

    Create separate database for testing

--- docker/mysql/create-databases.sql
+++ docker/mysql/create-databases.sql
@@ -3,3 +3,4 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 # create databases
 CREATE DATABASE IF NOT EXISTS `java-drones`;
 CREATE DATABASE IF NOT EXISTS `node-drones`;
+CREATE DATABASE IF NOT EXISTS `node-drones-test`;
